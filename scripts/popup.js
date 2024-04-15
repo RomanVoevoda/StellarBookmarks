425f358c92956725f86f20a6e8d9a929d1ef8f4e
Roman Druzhinin <karamount22@gmail.com>
2024-04-15 10:08:53 +0300

    feature: добавление скрипта popup.js
    *Создана кнопка Add new link, которая будет в каждой папке открывать форму создания ссылок
    *Добавлены прослушиватели кликов, которые открывают и закрывают формы создания папок и ссылок
    *Добавлен прослушиватель событий для режима удаления ссылок и папок

---
+++ scripts/popup.js
@@ -0,0 +1,46 @@
+const newFolderButton = document.querySelector('.fa-folder-plus');
+const deleteModeButton = document.querySelector('.fa-folder-minus');
+const createNewLinkButton = document.querySelector('.create-new-link-button');
+
+const newFolderFormCloseButton = document.querySelector('#folder-form-close-button');
+const newLinkFormCloseButton = document.querySelector('#link-form-close-button');
+
+const newFolderFormContainer = document.querySelector('.new-folder-form-container');
+const newLinkFormContainer = document.querySelector('.new-link-form-container');
+
+newFolderButton.addEventListener('click', () => {
+  newFolderFormContainer.classList.add('flex');
+});
+
+newFolderFormCloseButton.addEventListener('click', () => {
+  newFolderFormContainer.classList.remove('flex');
+});
+
+createNewLinkButton.addEventListener('click', () => {
+  newLinkFormContainer.classList.add('flex');
+});
+
+newLinkFormCloseButton.addEventListener('click', () => {
+  newLinkFormContainer.classList.remove('flex');
+});
+
+deleteModeButton.addEventListener('click', () => {
+  const linksTrashCans = document.getElementsByClassName('fa-trash-can');
+  if(linksTrashCans == null) {
+    return;
+  }
+
+  if(deleteModeButton.classList.contains('on')) {
+    deleteModeButton.classList.remove('on');
+
+    for(let trashCan of linksTrashCans) {
+      trashCan.classList.add('display-none');
+    }  
+  } else {
+    deleteModeButton.classList.add('on');
+
+    for(let trashCan of linksTrashCans) {
+      trashCan.classList.remove('display-none');
+    }  
+  }
+});
