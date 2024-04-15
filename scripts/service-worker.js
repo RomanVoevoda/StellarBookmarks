af7237cd5bae86c37b57f7ccbd77ae164290eb87
Roman Druzhinin <karamount22@gmail.com>
2024-04-15 13:37:31 +0300

    feature: улучшено взаимводействие с Chrome
    *Добавлен файл service-worker.js, который позволяет открывать приложение как боковую панель сразу по клику в интерфейсе Хрома

---
+++ scripts/service-worker.js
@@ -0,0 +1,6 @@
+const GOOGLE_ORIGIN = 'https://www.google.com';
+
+// Allows users to open the side panel by clicking on the action toolbar icon
+chrome.sidePanel
+  .setPanelBehavior({ openPanelOnActionClick: true })
+  .catch((error) => console.error(error));
