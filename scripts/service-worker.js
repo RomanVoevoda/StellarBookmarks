f8d4143f141c3cadbcf9cb8dfe6691de2dae33bd
Roman Druzhinin <karamount22@gmail.com>
2024-05-02 21:38:33 +0300

    feature: из плагина в веб-приложение
    *Убраны файлы для работы сайта в качестве плагина
    *Добавлены метатеги
    *Улучшен мобильный дизайн
    *Добавлена фавиконка

--- scripts/service-worker.js
+++
@@ -1,6 +0,0 @@
-const GOOGLE_ORIGIN = 'https://www.google.com';
-
-// Allows users to open the side panel by clicking on the action toolbar icon
-chrome.sidePanel
-  .setPanelBehavior({ openPanelOnActionClick: true })
-  .catch((error) => console.error(error));
